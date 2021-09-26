fb711739968dc2b06dfe3898fe9cc50e5b37ef28
Justin Figueroa <chillfig@gmail.com>
2021-09-26 18:30:55 -0500

    debugging commit

--- src/Sudoko.h
+++ src/Sudoko.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <set>
 const int puzzleDim = 9;    // [puzzleDim x puzzleDim] puzzle dimension
 
@@ -11,9 +12,14 @@ class puzzle{
                                                 // 0 if the puzzle contains repeated values
                                                 // 1 if the puzzle is valid
         
-        puzzle(){   // constructor that executes at object creation
-            cout << "This is printed automatically" << "\n\n";
-            acceptInput(numMatrix);
+        puzzle(char choice){   // constructor that executes at object creation
+            // if (choice == 'a'){
+            //     acceptInput(numMatrix);
+            // }
+            // else{
+            //     fileParse(numMatrix);
+            // }
+            fileParse(numMatrix);
             validity = validatePuzzle(numMatrix);
         }
 
@@ -38,41 +44,43 @@ class puzzle{
                     if(puzzleArray[i][j] < 0 || puzzleArray[i][j] > puzzleDim){
                         return -1;
                     }    
-                }
-                s.clear();  // clear the set s to allow new checks    
+                } 
             }
-            
+            s.clear(); 
             // check each row for duplicates in that row
             for(int i = 0; i < puzzleDim; i++){
                 for(int j = 0; j < puzzleDim; j++){
                     if(puzzleArray[i][j] != 0){
-                        if(s.insert(!puzzleArray[i][j]).second){
+                        if(s.insert(puzzleArray[i][j]).second == false){
+                            //cout << "The problem is with row duplicate. The value of matrix cell is: " << i << " " << j << ":  " << puzzleArray[i][j] << "\n";
                             return 0;
                         }
                     }    
                 }
                 s.clear(); // clear the set s to allow new checks for each row    
             }    
-            
+            s.clear(); 
             // check each column for duplicates in that column
             for(int i = 0; i < puzzleDim; i++){
                 for(int j = 0; j < puzzleDim; j++){
                     if(puzzleArray[j][i] != 0){
-                            if(s.insert(!puzzleArray[j][i]).second){
+                            if(s.insert(puzzleArray[j][i]).second == false){
+                                //cout << "The problem is with column duplicate. The value of matrix cell is: " << j << " " << i << ":  " << puzzleArray[j][i] <<"\n";
                                 return 0;
                             }
                         } 
                 }
                 s.clear(); // clear the set s to allow new checks for each column   
             }
-
+            s.clear(); 
             // check each 3x3 box for duplicates
-            for(int iBox = 0; iBox < 9; iBox + 3){
-                for(int jBox = 0; jBox < 9; jBox + 3){
+            for(int iBox = 0; iBox < 9; iBox += 3){
+                for(int jBox = 0; jBox < 9; jBox += 3){
                     for(int i = iBox; i < iBox + 3; i++){
                         for(int j = jBox; j < jBox + 3; j++){
                             if(puzzleArray[i][j] != 0){
-                                if(s.insert(!puzzleArray[i][j]).second){
+                                if(s.insert(puzzleArray[i][j]).second == false){
+                                    //cout << "The problem is with box duplicate. The duplicate value is: " << i << " " << j << ":  " << puzzleArray[i][j] << "\n";
                                     return 0;
                                 }
                             }
@@ -92,5 +100,34 @@ class puzzle{
                 }  
                 cout << "\n";
             }
-        }         
+        }
+
+        // Function that parses tokens from file and populates numMatrix 
+        int fileParse(int (&puzzleArray)[puzzleDim][puzzleDim]){
+            int number;
+            int i = 0;
+            int j = 0;
+            // File handling
+            string filename("puzzle.txt");
+            fstream input_file(filename);
+            if (!input_file.is_open()){
+                cerr << "Could not open the file - '"
+                     << filename << "'" << endl;
+                return EXIT_FAILURE;
+            }
+            // Load the puzzle array
+            while (input_file >> number){
+                puzzleArray[i][j] = number;
+                if(j == (puzzleDim - 1)){
+                    j = 0;
+                    i++;
+                }else{
+                    j++;
+                }
+
+            }
+            cout << endl;
+            input_file.close();
+            return EXIT_SUCCESS;
+        }
 };
